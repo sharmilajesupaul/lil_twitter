a6025ba63ce5b05c2d7451ce977d16c32d606409
Christopher Mamian & Ronald Chacon <pair+christophermamian.ronaldchacon@devbootcamp.com>
2014-11-20 16:13:03 -0800

    merge origin/usr-auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include BCrypt
   has_many :tweets
 
 
@@ -7,4 +8,19 @@ class User < ActiveRecord::Base
 
   has_many :followees, through: :followee_follows, source: :followee
   has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(typed_password)
+    return true if Password.new(self.hashed_password) == typed_password
+    false
+  end
+
 end
