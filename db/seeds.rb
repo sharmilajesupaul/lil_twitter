4f923efe4cfc0937f10ea036bd05c72fd48752ee
Jarred Hare <jarredhare@gmail.com>
2014-11-21 12:19:19 -0800

    Fixed seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,12 @@ require 'faker'
   # user.followers.create(username: Faker::Internet.user_name)
 end
 
+
 User.all.each do |user|
-  user.followers << User.all.sample(3)
+  3.times do
+    new_follower = User.all.sample
+    user.followers << new_follower unless new_follower == user
+  end
 end
 
 
