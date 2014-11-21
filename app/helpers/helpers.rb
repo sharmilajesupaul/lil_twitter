50c34c45ab89360c486b0ff0b63535f12fac81b5
Jarred Hare <jarredhare@gmail.com>
2014-11-21 15:24:33 -0800

    Fix follow button on own profile page, add twitter stats, fix gravatar issue

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,8 @@
 helpers do
 
   def gravatar_url(email)
+    email = 'bob@aol.com' if email.nil?
+
     email_hash = Digest::MD5.hexdigest(email.downcase)
     "http://www.gravatar.com/avatar/#{email_hash}"
   end
